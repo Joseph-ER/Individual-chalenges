2b09fb4ad0b27864ae99b38709053dc4db42bb15
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-22 12:26:23 +0100

    Added the bankTest file for the completed bank challenge

---
+++ bankTest/README.md
@@ -0,0 +1,8 @@
+Run 'npm install' to make sure dependencies are installed.
+
+I approached this task from the addition or subtraction of ammounts withdrawn or deposited before focusing on the key-value pairs for the display of the bank statement.
+
+I structured this code with as few functions as possible for better readability and to not overcomplicate the code.
+
+
+![Screenshot of app](https://imgur.com/P160F99)
---
+++ bankTest/bankTest.js
@@ -0,0 +1,41 @@
+class Bank{
+  constructor(balance){
+    this.balance = balance;
+    this.statement = [];
+  }
+
+  logBalance(){
+    return this.balance;
+  }
+
+  deposit(ammount, date){
+    this.balance += ammount;
+    this.statement.push({date: date, ammount: ammount, balance: this.balance});
+  }
+  
+  withdraw(ammount, date){
+    if (this.balance >= ammount){
+      this.balance -= ammount;
+      this.statement.push({date: date, ammount: ammount, balance: this.balance});
+    }else{
+      console.error('Not enough in account');
+    }
+  }
+
+  bankStatement(){
+    console.log('date ||   ammount   || balance');
+    this.statement.reverse(this.statement.date);
+    this.statement.forEach (action => {
+      console.log(`${action.date}||${action.ammount}||${action.balance}`);
+    });
+  }
+}
+
+const BankSys = new Bank(0);
+
+BankSys.deposit(1000,'10/01/2023');
+BankSys.deposit(2000,'13/01/2023');
+BankSys.withdraw(500,'14/012023');
+BankSys.bankStatement();
+
+module.exports = Bank;
