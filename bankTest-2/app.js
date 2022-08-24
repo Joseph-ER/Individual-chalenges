42146d1f5274ed94517c2868929801428b9972d5
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-24 12:32:13 +0100

    ammended app.js to be easier for user to use by runnig logic methods through bankDisplay instance

--- bankTest-2/app.js
+++ bankTest-2/app.js
@@ -1,9 +1,12 @@
 const Banklogic = require('./bankLogic');
 const BankDisplay = require('./bankDisplay');
 
+//Creation of the classes for users
 const bankLogic = new Banklogic(0);
-const bankDisplay = new BankDisplay(bankLogic);
-bankLogic.deposit(1000,'10/01/23');
-bankLogic.deposit(2000,'13/01/23');
-bankLogic.withdraw(500,'14/02/23');
-bankDisplay.showStatements();
+const bank = new BankDisplay(bankLogic);
+
+//Example usage of the app
+bank.logic.deposit(1000,'10/01/2023');
+bank.logic.deposit(2000,'13/01/2023');
+bank.logic.withdraw(500,'14/01/2023');
+bank.showStatements();
