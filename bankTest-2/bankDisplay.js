9dde46f5f644685fa7e0314a4e1771e00e4e0f33
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-24 12:17:30 +0100

    added each statement to output the statements individually

--- bankTest-2/bankDisplay.js
+++ bankTest-2/bankDisplay.js
@@ -6,7 +6,11 @@ class BankDisplay{
     this.logic.statements.reverse(this.logic.statements.date);
   }
   showStatements(){
+    this.orderStatements();
     console.log('date || credit ||  debit || balance');
+    this.logic.statements.forEach(statement => {
+      console.log(`${statement.date} || ${statement.credit} || ${statement.debit} || ${statement.balance} ||`);
+    })
   }
 
 }
