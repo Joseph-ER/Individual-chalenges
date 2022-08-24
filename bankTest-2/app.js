9464018f2cdd5dad8a07bbf4b45990de38091cd8
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-24 12:21:18 +0100

    added the example test transactions into app.js

--- bankTest-2/app.js
+++ bankTest-2/app.js
@@ -1,5 +1,9 @@
 const Banklogic = require('./bankLogic');
 const BankDisplay = require('./bankDisplay');
 
-const banklogic = new Banklogic(0);
-const bankDisplay = new BankDisplay(banklogic);
+const bankLogic = new Banklogic(0);
+const bankDisplay = new BankDisplay(bankLogic);
+bankLogic.deposit(1000,'10/01/23');
+bankLogic.deposit(2000,'13/01/23');
+bankLogic.withdraw(500,'14/02/23');
+bankDisplay.showStatements();
