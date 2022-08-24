17eb714f841a111e1fca45547907430f8908f994
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-24 14:59:11 +0100

    added a mock for the banklogic class int eh bankDisplay.spec.js file:

--- bankTest-2/bankDisplay.spec.js
+++ bankTest-2/bankDisplay.spec.js
@@ -1,19 +1,25 @@
-const BankDisplay = require("./bankDisplay")
-const BankLogic = require('./bankLogic');
+const BankDisplay = require("./bankDisplay");
+const BankLogic = require("./bankLogic");
+jest.mock('./bankLogic');
 
 describe('BankDisplay and BankLogic integration tests',()=>{
+  beforeEach(()=> {
+    BankLogic.mockClear();
+  })
   it('BankDisplay has access to BankLogic.statements',() => {
     const bankLogic = new BankLogic;
+    bankLogic.statements = [];
     const bank = new BankDisplay(bankLogic);
     expect(bank.logic.statements).toEqual([]);
   });
   it('orderStatements method reorders by date so 1st in array is most recent',() => {
-    const bankLogic = new BankLogic;
-    const bank = new BankDisplay(bankLogic);
-    bank.logic.deposit(100,'01/01/20');
-    bank.logic.deposit(100,'01/02/20');
-    expect(bank.logic.statements[0]).toEqual({"balance": 100, "credit": 100, "date": "01/01/20", "debit": 0})
+    const mockLogic = new BankLogic();
+    mockLogic.statements = [{balance: 100, date: '01/01/20'}, {balance: 100, date: '01/02/20'}];
+    console.log(mockLogic.statements);
+    const bank = new BankDisplay(mockLogic);
+    expect(bank.logic.statements[0]).toEqual({"balance": 100,"date": "01/01/20"})
     bank.orderStatements();
-    expect(bank.logic.statements[0]).toEqual({"balance": 100, "credit": 100, "date": "01/02/20", "debit": 0})
+    expect(bank.logic.statements[0]).toEqual({"balance": 100,"date": "01/02/20"})
   });
+  
 });
