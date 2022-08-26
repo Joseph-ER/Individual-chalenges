8ed46b79c5e6c8cb23930f3f478d6fba41aa5f7d
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-26 14:04:39 +0100

    Run ESLint on both class files and removed unnecessary use of JSON in tillLogic

--- tillTechTest/receiptLogic.js
+++ tillTechTest/receiptLogic.js
@@ -1,38 +1,38 @@
 class ReceiptLogic{
-  constructor(till){
-    this.till = till;
-    this.shopName = till.shop["shopName"];
-    this.shopAddress = till.shop["address"];
-    this.shopPhone = till.shop["phone"];
-    this.vat = 8.64;
-    this.calculatedvat = 0;
-  }
+	constructor(till){
+		this.till = till;
+		this.shopName = till.shop['shopName'];
+		this.shopAddress = till.shop['address'];
+		this.shopPhone = till.shop['phone'];
+		this.vat = 8.64;
+		this.calculatedvat = 0;
+	}
 
-  addVat(){
-    this.calculatedvat = (this.till.orderTotal/100)*this.vat;
-    this.till.orderTotal += this.calculatedvat;
-  }
+	addVat(){
+		this.calculatedvat = (this.till.orderTotal/100)*this.vat;
+		this.till.orderTotal += this.calculatedvat;
+	}
 
-  showReceipt(){
-    this.addVat();
-    console.log(this.receiptHeader());
-    console.log(this.receiptBody());
-  }
-  receiptHeader(){
-    return(`Date: ${this.till.date}\n${this.shopName}\n${this.shopAddress}\n${this.shopPhone}\n------------`);
-  }
+	showReceipt(){
+		this.addVat();
+		console.log(this.receiptHeader());
+		console.log(this.receiptBody());
+	}
+	receiptHeader(){
+		return(`Date: ${this.till.date}\n${this.shopName}\n${this.shopAddress}\n${this.shopPhone}\n------------`);
+	}
 
-  receiptBody(){
-    return(`Order: \n${this.till.order.join("\n")}\nVAT: £${this.calculatedvat.toFixed(2)}\nTotal: £${this.till.orderTotal.toFixed(2)}\n------------`);
-    // ${this.till.order.forEach(item=>{console.log(`${item}`)})}
-    // this.till.order.forEach(item=> {
-    //   console.log(`${item}`);
-    // })
-    // console.log('------------');
-    // console.log(`VAT: £${this.calculatedvat.toFixed(2)}`);
-    // console.log(`Total: £${this.till.orderTotal.toFixed(2)}`);
-    // console.log('------------');
-  }
+	receiptBody(){
+		return(`Order: \n${this.till.order.join('\n')}\nVAT: £${this.calculatedvat.toFixed(2)}\nTotal: £${this.till.orderTotal.toFixed(2)}\n------------`);
+		// ${this.till.order.forEach(item=>{console.log(`${item}`)})}
+		// this.till.order.forEach(item=> {
+		//   console.log(`${item}`);
+		// })
+		// console.log('------------');
+		// console.log(`VAT: £${this.calculatedvat.toFixed(2)}`);
+		// console.log(`Total: £${this.till.orderTotal.toFixed(2)}`);
+		// console.log('------------');
+	}
 
   
 }
--- tillTechTest/tillLogic.js
+++ tillTechTest/tillLogic.js
@@ -1,39 +1,38 @@
-const HipsterCoffee = require('./hipsterCoffee.json');
 class TillLogic{
-  constructor(shopDetails){
-    this.shop = JSON.stringify(shopDetails);
-    this.items = null;
-    this.menu = null
-    this.formatData();
-    this.order= [];
-    this.orderTotal = 0;
-    this.date = new Date(Date.now());
-  }
+	constructor(shopDetails){
+		this.shop = JSON.stringify(shopDetails);
+		this.items = null;
+		this.menu = null;
+		this.formatData();
+		this.order= [];
+		this.orderTotal = 0;
+		this.date = new Date(Date.now());
+	}
 
-  formatData(){
-    this.shop = JSON.parse(this.shop.slice(1,-1));
-    this.items = this.shop.prices;
-    this.items = JSON.parse(JSON.stringify(this.items).slice(1,-1));
-    this.menu = JSON.stringify(this.items).slice(1,-1);
-    this.menu = this.menu.split(",");
-  }
+	formatData(){
+		this.shop = JSON.parse(this.shop.slice(1,-1));
+		this.items = this.shop.prices;
+		this.items = JSON.parse(JSON.stringify(this.items).slice(1,-1));
+		this.menu = JSON.stringify(this.items).slice(1,-1);
+		this.menu = this.menu.split(',');
+	}
 
-  showMenu(){
-    console.log('------------');
-    console.log('Menu: ');
-    console.log('------------');
-    this.menu.forEach(item=>{
-      console.log(item);
-    });
-    console.log('------------');
-  }
+	showMenu(){
+		console.log('------------');
+		console.log('Menu: ');
+		console.log('------------');
+		this.menu.forEach(item=>{
+			console.log(item);
+		});
+		console.log('------------');
+	}
 
-  placeOrder(order, ammount){
-    for (let x = 0; x < ammount; x++){
-      this.order.push([order, this.items[order]]);
-      this.orderTotal += this.items[order];
-    }
-  }
+	placeOrder(order, ammount){
+		for (let x = 0; x < ammount; x++){
+			this.order.push([order, this.items[order]]);
+			this.orderTotal += this.items[order];
+		}
+	}
 }
 
 module.exports = TillLogic;
