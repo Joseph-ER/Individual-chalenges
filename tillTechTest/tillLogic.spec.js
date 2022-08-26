7d7c6db7edc6bdf54dc447e85ad4ceb7fc37ef7b
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-26 10:58:37 +0100

    Made sure all existing tests pass

--- tillTechTest/tillLogic.spec.js
+++ tillTechTest/tillLogic.spec.js
@@ -6,14 +6,14 @@ describe('TillLogic tests',() => {
     const till = new TillLogic(HipsterCoffee);
     expect(till.showMenu()).toEqual(HipsterCoffee.prices);
   });
-  // it('.menu can be viewed by index',() => {
-  //   const till = new TillLogic(HipsterCoffee);
-  //   expect(till.menu[0]).toEqual('"Cafe Latte":4.75');
-  // });
-  // it('orderItem adds the item named to the .order array',() => {
-  //   const till = new TillLogic(HipsterCoffee);
-  //   till.placeOrder('Caffe Latte');
-  //   expect(till.order[0]).toEqual([4.75]);
-  // })
+  it('.menu can be viewed by index',() => {
+    const till = new TillLogic(HipsterCoffee);
+    expect(till.menu[0]).toEqual('"Cafe Latte":4.75');
+  });
+  it('orderItem adds the item named to the .order array',() => {
+    const till = new TillLogic(HipsterCoffee);
+    till.placeOrder('Cafe Latte',1);
+    expect(till.order[0]).toEqual(['Cafe Latte',4.75]);
+  })
 
 })
